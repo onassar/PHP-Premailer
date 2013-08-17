1a1ff774647cee248ca0786385a42279b99ff487
Oliver Nassar <onassar@gmail.com>
2013-08-17 17:41:06 -0400

    Init

---
+++ Premailer.class.php
@@ -0,0 +1,143 @@
+<?php
+
+    // Ensure ruby script is executable
+    if (!is_executable(dirname(__FILE__) . '/converter.rb')) {
+        throw new Exception(
+            'converter.rb must be executable (eg. sudo chmod 0755 converter.rb)'
+        );
+    }
+
+    /**
+     * Premailer
+     * 
+     * @note    If ruby conversion file is named premailer.rb, it'll conflict
+     *          with the library
+     * @author  Oliver Nassar <onassar@gmail.com>
+     * @see     https://github.com/alexdunae/premailer/
+     * @see     http://premailer.dialect.ca/
+     */
+    class Premailer
+    {
+        /**
+         * _arguments
+         * 
+         * @note   Not all ruby options are supported (yet)
+         * @see    https://github.com/alexdunae/premailer/blob/master/lib/premailer/premailer.rb#L164
+         * @var    array
+         * @access protected
+         */
+        protected $_arguments = array(
+            'css_to_attributes' => true,
+            'include_link_tags' => true,
+            'include_style_tags' => true,
+            'input_encoding' => 'ASCII-8BIT',
+            'preserve_reset' => true,
+            'preserve_styles' => false,
+            'remove_classes' => true,
+            'remove_comments' => true,
+            'remove_ids' => true,
+            'remove_scripts' => true,
+            'replace_html_entities' => false
+        );
+
+        /**
+         * _markup
+         * 
+         * @var    string
+         * @access protected
+         */
+        protected $_markup;
+
+        /**
+         * _notSupported
+         * 
+         * Separate array to remind me of the outstanding arguments to support
+         * 
+         * @var    array
+         * @access protected
+         */
+        // protected $_notSupported = array(
+        //     'base_url' => nil,
+        //     'css' => [],
+        //     'css_string' => nil,
+        //     'debug' => false,
+        //     'line_length' => 65,
+        //     'link_query_string' => nil,
+        //     'verbose' => false
+        // );
+
+        /**
+         * __construct
+         * 
+         * @access public
+         * @param  string $markup
+         * @return void
+         */
+        public function __construct($markup)
+        {
+            $this->_markup = $markup;
+            $this->_arguments['with_html_string'] = true;
+        }
+
+        /**
+         * _getArgumentString
+         * 
+         * @see    http://rubyforge.org/docman/view.php/735/281/README.html
+         * @access protected
+         * @return string
+         */
+        protected function _getArgumentString()
+        {
+            // Set up the argument string, and escape the markup
+            $str = '';
+            $escapedMarkup = str_replace('"', '\"', $this->_markup);
+            $str .= '--markup "' . ($escapedMarkup) . '"';
+
+            // Loop over arguments; if boolean and true, set
+            foreach ($this->_arguments as $name => $value) {
+                if (is_bool($value)) {
+                    if ($value === true) {
+                        $str .= ' --' . ($name);
+                    }
+                } else {
+                    $str .= ' --' . ($name) . ' ' . ($value);
+                }
+            }
+
+            // Return for exec
+            return $str;
+        }
+
+        /**
+         * setArgument
+         * 
+         * @see    https://github.com/alexdunae/premailer/blob/master/lib/premailer/premailer.rb#L164
+         * @access public
+         * @param  string $name
+         * @param  string $value
+         * @return void
+         */
+        public function setArgument($name, $value)
+        {
+            $this->_arguments[$name] = $value;
+        }
+
+        /**
+         * getConvertedHtml
+         * 
+         * @access public
+         * @return string
+         */
+        public function getConvertedHtml()
+        {
+            $scriptPath = dirname(__FILE__) . '/converter.rb';
+            $output = array();
+            $returnVar = 0;
+            $command = ($scriptPath) . ' ' . $this->_getArgumentString();
+            $response = exec($command, $output, $returnVar);
+            if ($returnVar === 1) {
+                throw new Exception('Premailer or getopt gems not installed');
+            }
+            return implode('', $output);
+        }
+    }
---
+++ converter.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/ruby
+require 'rubygems'
+begin
+  gem 'premailer'
+  require 'premailer'
+  require 'getopt/long'
+
+  opt = Getopt::Long.getopts(
+    ['--markup', Getopt::REQUIRED],
+    ['--css_to_attributes', Getopt::BOOLEAN],
+    ['--include_link_tags', Getopt::BOOLEAN],
+    ['--include_style_tags', Getopt::BOOLEAN],
+    ['--input_encoding', Getopt::OPTIONAL],
+    ['--preserve_reset', Getopt::BOOLEAN],
+    ['--preserve_styles', Getopt::BOOLEAN],
+    ['--remove_classes', Getopt::BOOLEAN],
+    ['--remove_comments', Getopt::BOOLEAN],
+    ['--remove_ids', Getopt::BOOLEAN],
+    ['--remove_scripts', Getopt::BOOLEAN],
+    ['--replace_html_entities', Getopt::BOOLEAN],
+    ['--with_html_string', Getopt::BOOLEAN]
+  )
+  premailer = Premailer.new(
+      opt['markup'],
+      :css_to_attributes => opt['css_to_attributes'],
+      :include_link_tags => opt['include_link_tags'],
+      :include_style_tags => opt['include_style_tags'],
+      :input_encoding => opt['input_encoding'],
+      :preserve_reset => opt['preserve_reset'],
+      :preserve_styles => opt['preserve_styles'],
+      :remove_classes => opt['remove_classes'],
+      :remove_comments => opt['remove_comments'],
+      :remove_ids => opt['remove_ids'],
+      :remove_scripts => opt['remove_scripts'],
+      :replace_html_entities => opt['replace_html_entities'],
+      :with_html_string => opt['with_html_string']
+  )
+  puts premailer.to_inline_css
+rescue Gem::LoadError
+  raise 'Premailer not loaded'
+end
