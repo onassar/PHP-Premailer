c165d61f7af3f552cc5d2f98e4ea1b4c1e814973
Oliver Nassar <onassar@gmail.com>
2013-08-19 13:20:23 -0400

    Defaults for buggy paras

--- Premailer.class.php
+++ Premailer.class.php
@@ -37,9 +37,9 @@
             'input_encoding' => 'ASCII-8BIT',
             'preserve_reset' => true,
             'preserve_styles' => false,
-            'remove_classes' => true,// Some servers bugged outwhen this was on
+            'remove_classes' => false,// Some servers bugged outwhen this was on
             'remove_comments' => true,
-            'remove_ids' => true,// Some servers bugged outwhen this was on
+            'remove_ids' => false,// Some servers bugged outwhen this was on
             'remove_scripts' => true,
             'replace_html_entities' => false
         );
